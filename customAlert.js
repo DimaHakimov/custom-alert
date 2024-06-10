c9174361467985ee5a7c03b74d6bcf482cd87ae5
DimaHakimov <mitya.khakimov@internet.ru>
2024-06-10 20:25:19 +0700

    added custom design

--- customAlert.js
+++ customAlert.js
@@ -1,7 +1,11 @@
-function showCustomAlert(message, callBack){
- /*document.getElementById('mes1').innerText = message;*/
+function showCustomAlert(message, textOnButton,windowColor,messageColor,buttonColor,textButtonColor,callBack){
+ document.getElementById('mes1').innerText = String(message);
  document.getElementById('alertId').style.display = 'block';
-
+ document.getElementById('button1').innerText = String(textOnButton);
+ document.getElementById('alertContent').style.backgroundColor = 'green';
+ document.getElementById('alertContent').style.color = messageColor;
+ document.getElementById('button1').style.backgroundColor = buttonColor;
+ document.getElementById('button1').style.color = textButtonColor;
  document.getElementById('button1').onclick = function(){
     if(callBack){
         callBack();
@@ -12,4 +16,9 @@ function showCustomAlert(message, callBack){
 
 function closeCustomAlert(){
     document.getElementById('alertId').style.display = 'none';
+}
+
+
+window.onload = function(){
+    showCustomAlert('testMessage', 'Test', 'green','white','blue','yellow');
 }
